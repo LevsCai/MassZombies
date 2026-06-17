ae459f8ec7b88d075b0ae2aa79665d885c89b149
LevsCai <62013473+LevsCai@users.noreply.github.com>
2026-06-17 11:11:44 +0800

    Delete Plugins directory

--- Plugins/MassCommunitySample/Source/MassCommunitySample/ProjectileSim/MassProjectileHitInterface.cpp
+++
@@ -1,7 +0,0 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "MassProjectileHitInterface.h"
-
-
-// Add default functionality here for any IMassProjectileHitInterface functions that are not pure virtual.
